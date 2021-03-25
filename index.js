79c2543f4ade971124e0d45b55a1990579106a39
David McFadzean <davidmc@gmail.com>
2021-03-24 21:03:04 -0400

    Added getFile

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload')
 const fs = require('fs')
+const CID = require('cids')
+const uint8ArrayConcat = require('uint8arrays/concat')
+const uint8ArrayToString = require('uint8arrays/to-string')
 
 const ipfs = new ipfsClient({host:'localhost', port: '5001', protocol:'http'})
 const app = express()
@@ -18,33 +21,55 @@ app.get('/', (req,res) => {
 })
 
 app.post('/upload', (req, res) => {
-     const file = req.files.file
-     const fileName = req.body.fileName
-     const filePath =   'files/' + fileName
+    const file = req.files.file
+    const fileName = req.body.fileName
+    const filePath =   'files/' + fileName
 
-     file.mv(filePath, async (err) => {
-         if (err) {
+    file.mv(filePath, async (err) => {
+        if (err) {
             console.log('error: failed to download the file.')
             return res.status(500).send(err)
-         }
-     const fileHash = await addFile(fileName, filePath)
-     fs.unlink(filePath, (err) =>{
-         if (err) console.log(err)
-     })  
+        }
+     
+        const fileHash = await addFile(fileName, filePath)
+
+        fs.unlink(filePath, (err) => {
+            if (err) console.log(err)
+        })  
          
-     res.render('upload', {fileName, fileHash})
-     })
+        res.render('upload', {fileName, fileHash})
+    })
+})
+
+app.post('/download', (req, res) => {
+    const cid = new CID(req.body.cid)
+
+    const content = getFile(cid)
+             
+    res.render('home')
 })
 
 const addFile = async (fileName, filePath) => {
     const file = fs.readFileSync(filePath)
-   const fileAdded = await ipfs.add({path:fileName, content:file})
-  
+    const fileAdded = await ipfs.add({path:fileName, content:file})
+    const fileHash = fileAdded.cid
 
-
-   const fileHash = fileAdded.cid
-  console.log(fileAdded)
+    console.log(fileAdded)
     return fileHash
+}
+
+const getFile = async (cid) => {
+    
+    const chunks = []
+    
+    for await (const chunk of ipfs.cat(cid)) {
+        chunks.push(chunk)
+    }
+
+    console.log('Added file contents:', chunks)
+    data = uint8ArrayConcat(chunks)
+    console.log('Added file contents:', data.toString())
+    console.log('Added file contents:', uint8ArrayToString(data))
 }
 
 app.listen(3000,'127.0.0.1', () => {
